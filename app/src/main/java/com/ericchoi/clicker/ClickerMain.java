ac5295d5241abba6acf68f32dbc7b35b0aba5695
Eric Choi <eric.choi@sendgrid.com>
2014-07-06 17:43:53 -0700

    WIP for circle animation

--- app/src/main/java/com/ericchoi/clicker/ClickerMain.java
+++ app/src/main/java/com/ericchoi/clicker/ClickerMain.java
@@ -20,139 +20,147 @@ import android.widget.ImageView;
 public class ClickerMain extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
+  /**
+   * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+   */
+  private NavigationDrawerFragment mNavigationDrawerFragment;
+
+  /**
+   * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+   */
+  private CharSequence mTitle;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_clicker_main);
+
+    mNavigationDrawerFragment = (NavigationDrawerFragment)
+            getFragmentManager().findFragmentById(R.id.navigation_drawer);
+    mTitle = getTitle();
+
+    // Set up the drawer.
+    mNavigationDrawerFragment.setUp(
+            R.id.navigation_drawer,
+            (DrawerLayout) findViewById(R.id.drawer_layout));
+  }
+
+  @Override
+  public void onNavigationDrawerItemSelected(int position) {
+    // update the main content by replacing fragments
+    FragmentManager fragmentManager = getFragmentManager();
+    fragmentManager.beginTransaction()
+            .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+            .commit();
+  }
+
+  public void onSectionAttached(int number) {
+    switch (number) {
+      case 1:
+        mTitle = getString(R.string.title_section1);
+        break;
+      case 2:
+        mTitle = getString(R.string.title_section2);
+        break;
+      case 3:
+        mTitle = getString(R.string.title_section3);
+        break;
+    }
+  }
+
+  public void restoreActionBar() {
+    ActionBar actionBar = getActionBar();
+    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+    actionBar.setDisplayShowTitleEnabled(true);
+    actionBar.setTitle(mTitle);
+  }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    if (!mNavigationDrawerFragment.isDrawerOpen()) {
+      // Only show items in the action bar relevant to this screen
+      // if the drawer is not showing. Otherwise, let the drawer
+      // decide what to show in the action bar.
+      getMenuInflater().inflate(R.menu.clicker_main, menu);
+      restoreActionBar();
+      return true;
+    }
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+    if (id == R.id.action_settings) {
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  /**
+   * A placeholder fragment containing a simple view.
+   */
+  public static class PlaceholderFragment extends Fragment {
     /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     * The fragment argument representing the section number for this
+     * fragment.
      */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private Metronome metronome;
 
     /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     * Returns a new instance of this fragment for the given section
+     * number.
      */
-    private CharSequence mTitle;
+    public static PlaceholderFragment newInstance(int sectionNumber) {
+      PlaceholderFragment fragment = new PlaceholderFragment();
+      Bundle args = new Bundle();
+      args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+      fragment.setArguments(args);
+      return fragment;
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clicker_main);
-
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+    public PlaceholderFragment() {
     }
 
     @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      final View rootView = inflater.inflate(R.layout.fragment_clicker_main, container, false);
+      final Button button = (Button) rootView.findViewById(R.id.start_button);
+      button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+          ImageView needle = (ImageView) rootView.findViewById(R.id.metronome_needle);
+          Animation needleTurn = AnimationUtils.loadAnimation(v.getContext(), R.anim.needle_rotate);
+          //TODO
+          //needle.startAnimation(needleTurn);
+          metronome.startMetronome(v);
         }
-    }
+      });
 
-    public void restoreActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
+      ImageView leftCircle = (ImageView) rootView.findViewById(R.id.metronome_circle_left);
+      ImageView rightCircle = (ImageView) rootView.findViewById(R.id.metronome_circle_right);
+      //leftCircle.setAlpha (0.0f);
+      //rightCircle.setAlpha(0.0f);
 
+      this.metronome = new Metronome(
+              (ImageView) rootView.findViewById(R.id.metronome_circle_left),
+              (ImageView) rootView.findViewById(R.id.metronome_circle_right)
+      );
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.clicker_main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+      return rootView;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onAttach(Activity activity) {
+      super.onAttach(activity);
+      ((ClickerMain) activity).onSectionAttached(
+              getArguments().getInt(ARG_SECTION_NUMBER));
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-        private Metronome metronome;
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            final View rootView = inflater.inflate(R.layout.fragment_clicker_main, container, false);
-            final Button button = (Button) rootView.findViewById(R.id.start_button);
-            button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    ImageView needle = (ImageView) rootView.findViewById(R.id.metronome_needle);
-                    Animation needleTurn = AnimationUtils.loadAnimation(v.getContext(), R.anim.needle_rotate);
-                    needle.startAnimation(needleTurn);
-                    metronome.startMetronome(v);
-                }
-            });
-
-            this.metronome = new Metronome((ImageView) rootView.findViewById(R.id.metronome_circle_left));
-
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((ClickerMain) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-
-    }
-
+  }
 }
--- app/src/main/java/com/ericchoi/clicker/Metronome.java
+++ app/src/main/java/com/ericchoi/clicker/Metronome.java
@@ -11,39 +11,60 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Metronome {
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(100);
-    private ImageView circle;
-    private Animation circleFadeOut;
+  private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(100);
+  private ImageView leftCircle;
+  private Animation leftCircleFadeOut;
+  private ImageView rightCircle;
+  private Animation rightCircleFadeOut;
+  private AtomicBoolean isLeftsTurn;
 
-    public Metronome(ImageView circle) {
-        this.circle = circle;
-        this.circleFadeOut = AnimationUtils.loadAnimation(circle.getContext(), R.anim.circle_fade_out);
-    }
+  public Metronome(ImageView leftCircle, ImageView rightCircle) {
+    this.leftCircle = leftCircle;
+    this.leftCircleFadeOut = AnimationUtils.loadAnimation(leftCircle.getContext(), R.anim.circle_fade_out);
+    this.rightCircle = rightCircle;
+    this.rightCircleFadeOut = AnimationUtils.loadAnimation(rightCircle.getContext(), R.anim.circle_fade_out);
+    isLeftsTurn = new AtomicBoolean(true);
+  }
 
-    void startMetronome(final View v) {
-        final Runnable clicker = new Runnable() {
-            @Override
-            public void run() {
-                Log.v("metronome", "click!");
-                Activity a = (Activity)v.getContext();
-                a.runOnUiThread( new Runnable() {
-                    @Override
-                    public void run() {
-                        circle.startAnimation(circleFadeOut);
-                    }
-                });
-            }
-        };
+  void startMetronome(final View v) {
+    final Runnable clicker = new Runnable() {
+      @Override
+      public void run() {
+        Log.v("metronome", "click!");
+        final ImageView circle;
+        final Animation circleAnimation;
+        synchronized (isLeftsTurn) {
+          if (isLeftsTurn.get()) {
+            circle = leftCircle;
+            circleAnimation = leftCircleFadeOut;
+            isLeftsTurn.set(false);
+          }
+          else {
+            circle = rightCircle;
+            circleAnimation = rightCircleFadeOut;
+            isLeftsTurn.set(true);
+          }
+        }
+        Activity a = (Activity)v.getContext();
+        a.runOnUiThread( new Runnable() {
+          @Override
+          public void run() {
+            //circle.setAlpha(1.0f);
+            circle.startAnimation(circleAnimation);
+          }
+        });
+      }
+    };
 
-        final ScheduledFuture clickerHandle = scheduler.scheduleAtFixedRate(clicker, 0, 1, TimeUnit.SECONDS);
-        scheduler.schedule(new Runnable() {
-            public void run() {
-                //    clickerHandle.cancel(true);
-                Log.v("metronome", "cancel!");
-            }
-        }, 5, TimeUnit.SECONDS);
-    }
+    final ScheduledFuture clickerHandle = scheduler.scheduleAtFixedRate(clicker, 0, 1, TimeUnit.SECONDS);
+    scheduler.schedule(new Runnable() {
+      public void run() {
+        //    clickerHandle.cancel(true);
+        Log.v("metronome", "cancel!");
+      }
+    }, 5, TimeUnit.SECONDS);
+  }
 }
-
